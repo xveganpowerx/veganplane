624ae222c0190c0df4980508926ba33467d43f03
xveganpowerx <siberianvegansyndicate@gmail.com>
2014-06-28 23:56:33 +0700

    Add Standart assets mobile

--- VP/VP_Project/Assets/Scripts/playerControllerScript.cs
+++ VP/VP_Project/Assets/Scripts/playerControllerScript.cs
@@ -2,33 +2,40 @@
 using System.Collections;
 
 public class playerControllerScript : MonoBehaviour {
-	
+	private float leftBorder;
+	private float rightBorder;
+	public int playerSpeed = 5;
 	// Use this for initialization
+
+	void Awake() {
+		leftBorder = Camera.main.ViewportToWorldPoint(new Vector3(0,0,-10)).x;
+		rightBorder = Camera.main.ViewportToWorldPoint(new Vector3(1,0,-10)).x;
+	}
 	void Start () {
-		
+		print ("" + leftBorder + ";" + rightBorder);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			if (gameObject.transform.position.x > -6.65f) {
-				gameObject.transform.Translate(-Vector2.right * Time.deltaTime);
+			if (gameObject.transform.position.x > leftBorder) {
+				gameObject.transform.Translate(-Vector2.right * 5 * Time.deltaTime);
 			} else {
-				gameObject.transform.position =  new Vector2(-6.65f, gameObject.transform.position.y);
+				gameObject.transform.position =  new Vector2(leftBorder, gameObject.transform.position.y);
 			}
 		}
 		
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			if (gameObject.transform.position.x < 6.65f) {
-				gameObject.transform.Translate(Vector2.right * Time.deltaTime);
+			if (gameObject.transform.position.x < rightBorder) {
+				gameObject.transform.Translate(Vector2.right * 5 * Time.deltaTime);
 			} else {
-				gameObject.transform.position =  new Vector2(6.65f, gameObject.transform.position.y);
+				gameObject.transform.position =  new Vector2(rightBorder, gameObject.transform.position.y);
 			}
 		}
 		
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			if (gameObject.transform.position.y < 2) {
-				gameObject.transform.Translate(Vector2.up * Time.deltaTime);
+				gameObject.transform.Translate(Vector2.up * 5 * Time.deltaTime);
 			} else {
 				gameObject.transform.position =  new Vector2(gameObject.transform.position.x, 2);
 			}
@@ -36,7 +43,7 @@ public class playerControllerScript : MonoBehaviour {
 		
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			if (gameObject.transform.position.y > -1) {
-				gameObject.transform.Translate(-Vector2.up * Time.deltaTime);
+				gameObject.transform.Translate(-Vector2.up * 5 * Time.deltaTime);
 			} else {
 				gameObject.transform.position =  new Vector2(gameObject.transform.position.x, -1);
 			}
